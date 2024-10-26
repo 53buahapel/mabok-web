705021349090ec4221f92cd6ca681ce3c0f3b511
Rafi Nur Ardiansyah <rafimaster9@gmail.com>
2024-10-26 20:58:54 +0700

    fix: update port to port 80

--- ssrf/src/index.js
+++ ssrf/src/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
